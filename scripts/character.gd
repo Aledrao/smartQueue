8fe2712a6ef63a85bcb54c3f5251d8c8fa0299aa
Alex Sandro de Almeida Souza <aalex_sandro@hotmail.com>
2024-02-01 00:01:08 -0300

    Melhorias e correções no sistema de fila, melhorando o funcionamento e retirando entradas e saidas de areas3d.

--- scripts/character.gd
+++ scripts/character.gd
@@ -13,12 +13,14 @@ func _ready() -> void:
 	changeTarget = 0
 	navPosition = get_tree().get_root()
 	Caminhos.arrayCatracasFuncionais[0] = true
+	posicaoAtualFila = 0
 
 func _physics_process(delta: float) -> void:
-	if ((Caminhos.passandoCatraca == true) and (changeTarget == 1)) or posicaoAtualFila == 0:
+	if ((Caminhos.passandoCatraca == true) and (changeTarget == 1)):
 		navigation_agent.target_position = acessarFila()
 		#verificarProximaPosicaoFila()
 	else:
+		posicaoAtualFila = 0
 		navigation_agent.target_position = Caminhos.arrayCaminhosEntradaTelaTest[changeTarget]
 
 	var current_location = global_transform.origin
@@ -33,18 +35,18 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 		changeTarget += 1
 
 func acessarFila() -> Vector3:
-	print("POSIÇÃO NA FILA: ", posicaoAtualFila)
-	print("PRIMEIRO LUGAR: ", Caminhos.arrayOcupaPosicoesFilaEntrada[0])
+	print("POSICAO: ", posicaoAtualFila == 0)
+	print("CAMINHOS: ", Caminhos.ativarFilaEntrada)
+	print("RESPOSTAS: ", posicaoAtualFila == 0 and Caminhos.ativarFilaEntrada == false)
 	if posicaoAtualFila == 0:
 		for i in 11:
+			print("CONTADOR: ", i)
 			if(Caminhos.arrayOcupaPosicoesFilaEntrada[i] == false):
 				posicaoAtualFila = i + 1
-				Caminhos.arrayOcupaPosicoesFilaEntrada[i] == true
-			return Caminhos.arrayPosicaoFilaEntradaTelaTeste[i]
-	else:
-		return Caminhos.arrayPosicaoFilaEntradaTelaTeste[posicaoAtualFila - 1]
-
-	return Caminhos.arrayPosicaoFilaEntradaTelaTeste[9]
+				Caminhos.arrayOcupaPosicoesFilaEntrada[i] = true
+				print("LOCAL: ", Caminhos.arrayOcupaPosicoesFilaEntrada[i])
+				return Caminhos.arrayPosicaoFilaEntradaTelaTeste[i]
+	return Caminhos.arrayPosicaoFilaEntradaTelaTeste[posicaoAtualFila - 1]
 
 func verificarProximaPosicaoFila() -> void:
 	if posicaoAtualFila == 1:
