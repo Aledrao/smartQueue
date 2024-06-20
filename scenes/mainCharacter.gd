e3b3893abdf117c852c6650b59b93e7a0b8da684
Alex Sandro de Almeida Souza <aalex_sandro@hotmail.com>
2024-06-19 21:58:34 -0300

    Atualizacao dos itens do cenario, movimentacao do personagem principal e sua camera.

--- scenes/mainCharacter.gd
+++ scenes/mainCharacter.gd
@@ -1,12 +1,18 @@
 extends CharacterBody3D
 
+@onready var camera := $Neck/Camera3D2
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var look_dir: Vector2
+var camera_sens = 50
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,7 +25,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -28,4 +34,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	_rotate_camera(delta)
 	move_and_slide()
+
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion: look_dir = event.relative * 0.01
+
+func _rotate_camera(delta: float, sens_mod: float = 1.0):
+	var input = Input.get_vector("look_left", "look_right", "look_down", "look_up")
+	look_dir += input
+	rotation.y -= look_dir.x * camera_sens * delta
+	camera.rotation.x = clamp(camera.rotation.x - look_dir.y * camera_sens * sens_mod * delta, -1.5, 1.5)
+	look_dir = Vector2.ZERO
